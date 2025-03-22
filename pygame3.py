d623b4f06569bf2d7d4d7e485402c5e1653ba101
nazer7 <nazerkebekbosyn932@gmail.com>
2025-03-22 09:33:05 +0300

    Add files via upload

--- pygame3.py
+++ pygame3.py
@@ -1,7 +1,30 @@
-import pygame 
+import pygame
 pygame.init()
-screen = pygame.display.set_mode((600,400))
-pygame.display.set_caption("task3")
-while 
+WIDTH, HEIGHT = 500, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moving Red Ball")
+BALL_RADIUS = 25
+ball_x, ball_y = WIDTH // 2, HEIGHT // 2
+BALL_COLOR = (255, 0, 0)
+BACKGROUND_COLOR = (255, 255, 255)
+STEP = 20
+running = True
+while running:
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.circle(screen, BALL_COLOR, (ball_x, ball_y), BALL_RADIUS)
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and ball_y - BALL_RADIUS - STEP >= 0:
+                ball_y -= STEP
+            elif event.key == pygame.K_DOWN and ball_y + BALL_RADIUS + STEP <= HEIGHT:
+                ball_y += STEP
+            elif event.key == pygame.K_LEFT and ball_x - BALL_RADIUS - STEP >= 0:
+                ball_x -= STEP
+            elif event.key == pygame.K_RIGHT and ball_x + BALL_RADIUS + STEP <= WIDTH:
+                ball_x += STEP
+pygame.quit()
 
 
